aebbd1a9fba14211d207c6b4d851dc8ffdc365d0
Andreas Beyl <beyl@stimme.net>
2013-07-23 11:09:17 +0200

    added/modified tsconfig-files

---
+++ Configuration/TSconfig/Page/page.11.tsconfig.ts
@@ -0,0 +1,16 @@
+# Nur Datensaetze fuer News und Newskategorie erlauben
+#mod.web_list.allowedNewTables = tt_news,tt_news_cat
+
+### Seitencache von Ausgabeseiten löschen ###
+TCEMAIN.clearCacheCmd = 12,13
+
+### Beschreibung anpassen ###
+#TCEFORM.tt_news.short.label.default = Anreisser-Text
+#TCEFORM.tt_news.short.label.de = Anreisser-Text
+
+### News-Typen entfernen ###
+# 0 - 'News'
+# 1 - 'Link internal Page'
+# 2 - 'Link external URL'
+# 3 - 'Blog'
+#TCEFORM.tt_news.type.removeItems = 1,2,3
---
+++ Configuration/TSconfig/Page/page.16.tsconfig.ts
@@ -0,0 +1,2 @@
+# Nur Datensaetze fuer Backend-Layouts erlauben
+mod.web_list.allowedNewTables = backend_layout, tx_gridelements_backend_layout
