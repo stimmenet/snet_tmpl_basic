532fb795c726a8fa2eea10b578394677cf3908ba
Andreas Beyl <beyl@stimme.net>
2013-10-14 16:48:43 +0200

    added lightbox-support for news-images

--- Resources/Public/Scripts/JS/Swipebox/enable_swipebox.js
+++ Resources/Public/Scripts/JS/Swipebox/enable_swipebox.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function() {
-    jQuery('a.lightbox').swipebox({
+    jQuery('a.lightbox, div.mediaelement-image > a').swipebox({
         useCSS : true,      // false will force the use of jQuery for animations
         hideBarsDelay : 0   // 0 to always show caption and action bar
     });
