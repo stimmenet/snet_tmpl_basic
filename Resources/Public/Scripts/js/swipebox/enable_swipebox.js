317582397805fc7280cf741de5c92c742d70c6fd
Andreas Beyl <beyl@stimme.net>
2013-10-14 16:17:50 +0200

    removed Scripts-subfolders

--- Resources/Public/Scripts/js/swipebox/enable_swipebox.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-    jQuery('a.lightbox').swipebox({
-        useCSS : true,      // false will force the use of jQuery for animations
-        hideBarsDelay : 0   // 0 to always show caption and action bar
-    });
-});
