f9bb289345c1770d1a7e13b690f7e0d36b4018c2
Andreas Beyl <beyl@stimme.net>
2014-01-09 14:39:13 +0100

    moved realurl-config to ext_localconf

---
+++ ext_localconf.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ *
+ * default realurl configuration
+ *
+ */
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['realurl'] = array (
+	'_DEFAULT' => array (
+		'init' => array (
+			'enableCHashCache' => '1',
+			'appendMissingSlash' => 'ifNotFile',
+			'enableUrlDecodeCache' => '1',
+			'enableUrlEncodeCache' => '1',
+		),
+		'redirects' => array (
+			'sitemap.xml' => 'index.php?eID=dd_googlesitemap',
+			'news-sitemap.xml' => 'index.php?eID=dd_googlesitemap&sitemap=news&pidList=11&singlePid=13'
+		),
+		'preVars' => array (
+			array(
+				'GETvar' => 'no_cache',
+				'valueMap' => array(
+					'no_cache' => 1,
+				),
+				'noMatch' => 'bypass',
+			),
+			array(
+				'GETvar' => 'L',
+				'valueMap' => array(
+					'de' => '0',
+					'en' => '1',
+				),
+				'valueDefault' => 'de',
+				'valueDescription' => array(
+					'de' => 'deutsch',
+					'en' => 'english',
+				),
+				'noMatch' => 'bypass',
+			),
+		),
+		'pagePath' => array (
+			'type' => 'user',
+			'userFunc' => 'EXT:realurl/class.tx_realurl_advanced.php:&tx_realurl_advanced->main',
+			'spaceCharacter' => '-',
+			'languageGetVar' => 'L',
+			'expireDays' => '7',
+			'rootpage_id' => '1',
+		),
+		'fixedPostVars' => array (
+			'newsDetailConfiguration' => array(
+				array(
+					'GETvar' => 'tx_news_pi1[news]',
+					'lookUpTable' => array(
+						'table' => 'tx_news_domain_model_news',
+						'id_field' => 'uid',
+						'alias_field' => 'title',
+						'addWhereClause' => ' AND NOT deleted',
+						'useUniqueCache' => 1,
+						'useUniqueCache_conf' => array(
+							'strtolower' => 1,
+							'spaceCharacter' => '-'
+						)
+					)
+				)
+			),
+			'13' => 'newsDetailConfiguration',
+		),
+		'postVarSets' => array (
+			'_DEFAULT' => array (
+				'controller' => array(
+					array(
+						'GETvar' => 'tx_news_pi1[action]',
+						'noMatch' => 'bypass'
+					),
+					array(
+						'GETvar' => 'tx_news_pi1[controller]',
+						'noMatch' => 'bypass'
+					),
+				),
+			),
+		),
+		'fileName' => array (
+			//
+			// if you don't want .html-URLs set the following to "false" (e.g. 'defaultToHTMLsuffixOnPrev' => false,)
+			// then you get http://www.yourdomain.com/imprint/ instead of http://www.yourdomain.com/imprint.html
+			//
+			'defaultToHTMLsuffixOnPrev' => true,
+			'index' => array (
+				'feed.rss' => array(
+					'keyValues' => array(
+						'type' => 9818,
+					)
+				),
+			),
+		),
+	),
+
+);
+?>
