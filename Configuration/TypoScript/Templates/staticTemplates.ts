75c074b1cbf88ed4cf134332b7da10bd2532e905
Andreas Beyl <beyl@stimme.net>
2014-09-04 15:58:17 +0200

    replaced tq_seo with metaseo

--- Configuration/TypoScript/Templates/staticTemplates.ts
+++ Configuration/TypoScript/Templates/staticTemplates.ts
@@ -3,5 +3,5 @@
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:form/Configuration/TypoScript/setup.txt">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:gridelements/Configuration/TypoScript/setup.txt">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:wt_spamshield/Configuration/TypoScript/setup.txt">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:tq_seo/Configuration/TypoScript/setup.txt">
+<INCLUDE_TYPOSCRIPT: source="FILE:EXT:metaseo/Configuration/TypoScript/setup.txt">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:news/Configuration/TypoScript/setup.txt">
