d03b47e3b4e3be9a5928ff86bd13448b454d35e7
Sonja Zintz <zintz@stimme.net>
2015-03-20 15:29:19 +0100

    removed inclusion of static constants template of sysext form

--- Configuration/TypoScript/Templates/staticTemplates.constants.ts
+++ Configuration/TypoScript/Templates/staticTemplates.constants.ts
@@ -1,6 +1,5 @@
 # TypoScript constants coming from third-party extensions.
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:css_styled_content/static/constants.txt">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:form/Configuration/TypoScript/constants.txt">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:gridelements/Configuration/TypoScript/constants.txt">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:wt_spamshield/Configuration/TypoScript/constants.txt">
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:metaseo/Configuration/TypoScript/constants.txt">
